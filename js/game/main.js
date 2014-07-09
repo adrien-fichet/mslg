c9311bc4ad63f4bb83993ccb839c9d08bd2bec2c
sniksnp <sniksnp@gmail.com>
2014-07-09 17:52:53 +0200

    Add a star sprite

--- js/game/main.js
+++ js/game/main.js
@@ -1,12 +1,18 @@
 window.onload = function() {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
     function preload () {
-        game.load.image('logo', 'im/phaser.png');
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('ground', 'assets/platform.png');
+        game.load.image('star', 'assets/star.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
 
     function create () {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        game.add.sprite(0, 0, 'star');
+    }
+
+    function update() {
+
     }
 };
