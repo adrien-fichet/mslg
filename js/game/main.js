9dbd6abb85209de4cdecf1dfaa818f6ddbefadc0
sniksnp <sniksnp@gmail.com>
2014-07-09 18:13:23 +0200

    Move the player with arrow keys

--- js/game/main.js
+++ js/game/main.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     var game = new Phaser.Game(600, 400, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var platforms;
+    var player;
+    var cursors
 
     function preload () {
         game.load.image('sky', 'assets/sky.png');
@@ -24,18 +27,39 @@ window.onload = function() {
         ledge = platforms.create(-150, 200, 'ground');
         ledge.body.immovable = true;
 
-        var player = game.add.sprite(32, game.world.height - 150, 'dude');
+        player = game.add.sprite(32, game.world.height - 150, 'dude');
         game.physics.arcade.enable(player);
 
         player.body.bounce.y = 0.2;
-        player.body.gravity.y = 300;
+        player.body.gravity.y = 1000;
         player.body.collideWorldBounds = true;
 
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        cursors = game.input.keyboard.createCursorKeys();
     }
 
     function update() {
+        game.physics.arcade.collide(player, platforms);
 
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown) {
+            player.body.velocity.x = -150;
+            player.animations.play('left');
+
+        } else if (cursors.right.isDown) {
+            player.body.velocity.x = 150;
+            player.animations.play('right');
+
+        } else {
+            player.animations.stop();
+            player.frame = 4;
+        }
+
+        if (cursors.up.isDown && player.body.touching.down) {
+            player.body.velocity.y = -300;  // jump
+        }
     }
 };
