9813fd15e7fe71ab554932fa337c785e91e5a30f
sniksnp <sniksnp@gmail.com>
2014-07-09 18:02:43 +0200

    Add player

--- js/game/main.js
+++ js/game/main.js
@@ -24,7 +24,15 @@ window.onload = function() {
         ledge = platforms.create(-150, 200, 'ground');
         ledge.body.immovable = true;
 
-        
+        var player = game.add.sprite(32, game.world.height - 150, 'dude');
+        game.physics.arcade.enable(player);
+
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
     }
 
     function update() {
